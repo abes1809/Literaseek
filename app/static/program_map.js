7b69f53eaf2ac45d9178d6cf5ca25fc46c6feb4d
abes1809 <amandanicole321@gmail.com>
2019-12-01 17:37:15 -0600

    added columns to all programs page

--- app/static/program_map.js
+++ app/static/program_map.js
@@ -1,7 +1,7 @@
 $(function(){
 
 	/** base map */
-	var map = L.map('program_map').setView([41.881832, -87.623177], 13);
+	var map = L.map('program_map').setView([41.881832, -87.623177], 11);
 
     var Stamen_Terrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
 	   attribution: 'Stamen',
